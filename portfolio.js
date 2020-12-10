7d219ce2edbcbd1173b67d1e16bc1dbb1b106380
Mark Birchall <mark.birchall@gmail.com>
2020-12-10 17:00:41 +0000

    improve ux of animation options

--- portfolio.js
+++ portfolio.js
@@ -12,12 +12,14 @@ const portfolio = {
 const balls = {
     redVal: 255,
     greenVal: 255,
-    blueVal: 255,
-    amplitude: 0.25,
-    frequency: 0.25,
-    offset: 0.5,
-    transparency: 0.025,
-    spread: 50
+    blueVal: 50,
+    amplitude: 0.1,
+    frequency: 2,
+    offsetX: 0.8,
+    offsetY: 0,
+    transparency: 0.8,
+    fade: -0.5,
+    spread: 10
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -42,8 +44,16 @@ const navbarClassArr = ['.menu_home', '.menu_portfolio', '.menu_about', '.menu_c
 const navbarLinksArr = [];
 
 const handleMenuChoice = (e) => {
-    navbarLinksArr.forEach((link) => {
+    navbarLinksArr.forEach((link,index) => {
         if (link.classList.contains('current')) link.classList.remove('current');
+        if(link===e){
+            
+            //move animation window 
+            portfolio.animWindow.style.top = `${60 + (935 * index)}px`;
+
+            console.log(index);
+            console.log(portfolio.animWindow.style.top);
+        }
     })
     e.classList.add('current')
 }
@@ -59,7 +69,7 @@ navbarClassArr.forEach((link, index) => {
 const setupAnimatingElements = () => {
     portfolio.pageTop = document.querySelector('.pageTop');
     let objectsArr = [];
-    for (i = 0; i < 20; i++) {
+    for (i = 0; i < 80; i++) {
         let newObject = document.createElement('div');
         newObject.style.display = "block";
         newObject.style.zIndex = "999";
@@ -75,11 +85,13 @@ const setupAnimatingElements = () => {
 
 
 const setupAnimationOptions = () => {
-    
-    
 
     portfolio.animWindow = document.querySelector('.anim_options');
-    console.log(portfolio.animWindow);
+
+    const closeButton = document.querySelector('.close');
+    closeButton.onclick = () => {
+        portfolio.animWindow.style.display = "none";
+    };
 
     const iconSettings = document.querySelector('.icon_settings');
     iconSettings.onclick = () => {
@@ -88,58 +100,77 @@ const setupAnimationOptions = () => {
     };
 
     const sliderAmplitude = document.querySelector('.slider.amplitude');
+    sliderAmplitude.defaultValue = balls.amplitude * 100;
     sliderAmplitude.oninput = () => {
         balls.amplitude = sliderAmplitude.value / 100;
     }
     const sliderFrequency = document.querySelector('.slider.frequency');
+    sliderFrequency.defaultValue = balls.frequency * 100;
     sliderFrequency.oninput = () => {
         balls.frequency = sliderFrequency.value / 100;
     }
-    const sliderOffset = document.querySelector('.slider.offset');
-    sliderOffset.oninput = () => {
-        balls.offset = sliderOffset.value / 100;
+    const sliderOffsetX = document.querySelector('.slider.offsetX');
+    sliderOffsetX.defaultValue = balls.offsetX * 100;
+    sliderOffsetX.oninput = () => {
+        balls.offsetX = sliderOffsetX.value / 100;
+    }
+    const sliderOffsetY = document.querySelector('.slider.offsetY');
+    sliderOffsetY.defaultValue = balls.offsetY * 100;
+    sliderOffsetY.oninput = () => {
+        balls.offsetY = sliderOffsetY.value /100;
     }
     const sliderSpread = document.querySelector('.slider.spread');
+    sliderSpread.defaultValue = balls.spread;
     sliderSpread.oninput = () => {
         balls.spread = sliderSpread.value;
     }
     const sliderColRed = document.querySelector('.slider.red');
+    sliderColRed.defaultValue = balls.redVal;
     sliderColRed.oninput = () => {
         balls.redVal = sliderColRed.value;
     }
     const sliderColGreen = document.querySelector('.slider.green');
+    sliderColGreen.defaultValue = balls.greenVal;
     sliderColGreen.oninput = () => {
         balls.greenVal = sliderColGreen.value;
     }
     const sliderColBlue = document.querySelector('.slider.blue');
+    sliderColBlue.defaultValue = balls.blueVal;
     sliderColBlue.oninput = () => {
         balls.blueVal = sliderColBlue.value;
     }
-    
     const sliderColOpacity = document.querySelector('.slider.opacity');
+    sliderColOpacity.defaultValue = (1.0 - balls.transparency) * 1000;
     sliderColOpacity.oninput = () => {
-        balls.transparency = (500 - sliderColOpacity.value) /1000;
+        balls.transparency = (1000 - sliderColOpacity.value) / 1000;
     }
-
+    const sliderColFade = document.querySelector('.slider.fade');
+    sliderColFade.defaultValue = balls.fade * 500;
+    sliderColFade.oninput = () => {
+        balls.fade = -1.0 * sliderColFade.value / 500;
+    }
 }
 
 const animate = () => {
-    const animObjects = document.querySelectorAll('.object');
+    const animObjectsArr = document.querySelectorAll('.object');
     const pageWidth = document.body.clientWidth;
-    animObjects.forEach((item, index) => {
-        const position = (pageWidth * balls.amplitude) * Math.sin(portfolio.animStep + index * balls.frequency) + (pageWidth * balls.offset) - 20
+    animObjectsArr.forEach((item, index) => {
+        const sinVal = Math.sin(portfolio.animStep + index * balls.frequency)
+        const position = (pageWidth * balls.amplitude) * sinVal + (pageWidth * balls.offsetX) - 20
+        item.style.zIndex = "1";
         item.style.left = `${position}px`;
-        item.style.top = `${balls.spread * index}px`;
-        item.style.backgroundColor = `rgba(${balls.redVal},${balls.greenVal},${balls.blueVal},${0.5 - (balls.transparency)})`
+        item.style.top = `${(balls.offsetY*2810) + (balls.spread * index)}px`;
+        item.style.backgroundColor = `rgba(${balls.redVal},${balls.greenVal},${balls.blueVal},${1.0 - ((balls.transparency) + (sinVal * balls.fade))})`
+
     })
     portfolio.animStep += portfolio.animResolution;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    
+
     setupAnimatingElements();
-    
+
 
     setupAnimationOptions();
 
